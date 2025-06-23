12dd7bd94df586a66f4dba1114ae2946d81f4049
Mahum Maqbool <mahummaqbool@egora.pk>
2025-06-23 16:25:46 +0500

    Build: add js output for library consumers

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,18 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button["default"];
+  }
+});
+Object.defineProperty(exports, "Card", {
+  enumerable: true,
+  get: function get() {
+    return _Card["default"];
+  }
+});
 var _Card = _interopRequireDefault(require("./components/Card"));
 var _Button = _interopRequireDefault(require("./components/Button"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var _default = exports["default"] = {
-  Card: _Card["default"],
-  Button: _Button["default"]
-};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Card from './components/Card';
 import Button from './components/Button';
 
-export default { Card, Button };
+export { Card, Button };
