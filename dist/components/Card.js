e3f5c8b4f37fd6c4b4f6d869f4ff0ecaf8f6af9c
Mahum Maqbool <mahummaqbool@egora.pk>
2025-06-23 14:42:32 +0500

    Build: add js output for library consumers

--- dist/components/Card.js
+++ dist/components/Card.js
@@ -6,25 +6,16 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
-function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
-function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var Card = function Card(_ref) {
   var title = _ref.title,
-    description = _ref.description,
-    children = _ref.children,
-    _ref$style = _ref.style,
-    style = _ref$style === void 0 ? {} : _ref$style;
+    description = _ref.description;
   return /*#__PURE__*/_react["default"].createElement("div", {
-    style: _objectSpread({
+    style: {
       border: '1px solid #ddd',
       borderRadius: '8px',
       padding: '16px',
       boxShadow: '0 2px 8px rgba(0,0,0,0.1)'
-    }, style)
+    }
   }, title && /*#__PURE__*/_react["default"].createElement("h2", {
     style: {
       margin: '0 0 10px'
@@ -33,6 +24,6 @@ var Card = function Card(_ref) {
     style: {
       marginBottom: '10px'
     }
-  }, description), children);
+  }, description));
 };
 var _default = exports["default"] = Card;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const Card = ({ title, description, children, style = {} }) => {
+const Card = ({ title, description}) => {
   return (
     <div style={{
       border: '1px solid #ddd',
       borderRadius: '8px',
       padding: '16px',
       boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
-      ...style
     }}>
       {title && <h2 style={{ margin: '0 0 10px' }}>{title}</h2>}
       {description && <p style={{ marginBottom: '10px' }}>{description}</p>}
-      {children}
     </div>
   );
 };
