95504c42507554557f706815afa476216caa88c4
Mahum Maqbool <mahummaqbool@egora.pk>
2025-06-23 16:11:20 +0500

    Build: add js output for library consumers

--- dist/components/index.js
+++ dist/components/index.js
@@ -3,6 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button["default"];
+  }
+});
 Object.defineProperty(exports, "Card", {
   enumerable: true,
   get: function get() {
@@ -10,4 +16,5 @@ Object.defineProperty(exports, "Card", {
   }
 });
 var _Card = _interopRequireDefault(require("./components/Card"));
+var _Button = _interopRequireDefault(require("./components/Button"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
